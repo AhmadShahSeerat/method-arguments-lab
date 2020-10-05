ca4295879ea2abfe81d7fccdde91c5ee6b2a3504
iamShah <formal>
2020-10-05 10:28:20 -0400

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,6 @@
-def introduction (name, language)
-    puts "hi my name #{name} is and I'm to program #{language}."
+def introduction_with_language(name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+def introduction(name)
+    puts "Hi, my name is #{name}."
 end
